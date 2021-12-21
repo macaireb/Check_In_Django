4211863489fbba04b1704ad9930d8c16df2ac17c
macaireb <macairealexander@gmail.com>
2021-12-21 15:01:27 -0500

    changed to postgres

--- Check_In_Django/settings.py
+++ Check_In_Django/settings.py
@@ -82,8 +82,14 @@ WSGI_APPLICATION = 'Check_In_Django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'check_in_db',
+        'USER': 'postgres',
+        'PASSWORD': 'athlonx2',
+        'HOST': 'localhost',
+        'PORT': '', 
     }
 }
 
